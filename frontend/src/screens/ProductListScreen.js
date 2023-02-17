e7d1e7d41e8b14ee74feb04d032039bc7944745f
rayiqi <vindbaiq@gmail.com>
2023-02-17 13:48:51 +0700

    first commit

---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,210 @@
+import React, { useContext, useEffect, useReducer } from "react";
+import axios from "axios";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { toast } from "react-toastify";
+import { Store } from "../Store";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import Card from "react-bootstrap/Card";
+import { getError } from "../utils";
+import Rating from "../components/Rating";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        products: action.payload.products,
+        page: action.payload.page,
+        pages: action.payload.pages,
+        loading: false,
+      };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    case "CREATE_REQUEST":
+      return { ...state, loadingCreate: true };
+    case "CREATE_SUCCESS":
+      return {
+        ...state,
+        loadingCreate: false,
+      };
+    case "CREATE_FAIL":
+      return { ...state, loadingCreate: false };
+
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true, successDelete: false };
+    case "DELETE_SUCCESS":
+      return {
+        ...state,
+        loadingDelete: false,
+        successDelete: true,
+      };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false, successDelete: false };
+
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
+    default:
+      return state;
+  }
+};
+
+export default function ProductListScreen() {
+  const [
+    {
+      loading,
+      error,
+      products,
+      pages,
+      loadingCreate,
+      loadingDelete,
+      successDelete,
+    },
+    dispatch,
+  ] = useReducer(reducer, {
+    loading: true,
+    error: "",
+  });
+
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const page = sp.get("page") || 1;
+
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/admin?page=${page} `, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {}
+    };
+
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    }
+  }, [page, userInfo, successDelete]);
+
+  const createHandler = async () => {
+    if (window.confirm("Are you sure to create?")) {
+      try {
+        dispatch({ type: "CREATE_REQUEST" });
+        const { data } = await axios.post(
+          "/api/products",
+          {},
+          {
+            headers: { Authorization: `Bearer ${userInfo.token}` },
+          }
+        );
+        toast.success("product created successfully");
+        dispatch({ type: "CREATE_SUCCESS" });
+        navigate(`/admin/product/${data.product._id}`);
+      } catch (err) {
+        toast.error(getError(error));
+        dispatch({
+          type: "CREATE_FAIL",
+        });
+      }
+    }
+  };
+
+  const deleteHandler = async (product) => {
+    if (window.confirm("Are you sure to delete?")) {
+      try {
+        await axios.delete(`/api/products/${product._id}`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        toast.success("product deleted successfully");
+        dispatch({ type: "DELETE_SUCCESS" });
+      } catch (err) {
+        toast.error(getError(error));
+        dispatch({
+          type: "DELETE_FAIL",
+        });
+      }
+    }
+  };
+
+  const addToCartHandler = () => {};
+
+  return (
+    <div>
+      <Row>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+      </Row>
+
+      {loadingCreate && <LoadingBox></LoadingBox>}
+      {loadingDelete && <LoadingBox></LoadingBox>}
+
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          <div className="products">
+            <Row>
+            {products.map((product) => (
+              <Col>
+              <Card>
+                <Link to={`/product/${product.slug}`}>
+                  <img
+                    src={product.image} 
+                    className="card-img-top img-smal"
+                    alt={product.name}
+                  />
+                </Link>
+                <Card.Body>
+                  <Link to={`/product/${product.slug}`}>
+                    <Card.Title>{product.name}</Card.Title>
+                  </Link>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  />
+                  <Card.Text>${product.price}</Card.Text>
+                  {product.countInStock === 0 ? (
+                    <Button variant="light" disabled>
+                      Out of stock
+                    </Button>
+                  ) : (
+                    <Button onClick={() => addToCartHandler(product)}>
+                      Add to cart
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+              </Col>
+            ))}
+          </Row>
+          </div>
+          <div>
+            {[...Array(pages).keys()].map((x) => (
+              <Link
+                className={x + 1 === Number(page) ? "btn text-bold" : "btn"}
+                key={x + 1}
+                to={`/admin/products?page=${x + 1}`}
+              >
+                {x + 1}
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
